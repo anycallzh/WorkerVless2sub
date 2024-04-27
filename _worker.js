f6350703efaa5a7de77ea21a4546f85098f4d587
anycallzh <149305272+anycallzh@users.noreply.github.com>
2024-04-27 20:35:01 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -39,7 +39,9 @@ let link = '';
 let edgetunnel = 'ed';
 let RproxyIP = 'false';
 let proxyIPs = [
-	'proxyip.us.prianganmedia.eu.org',
+	'proxyip.aliyun.fxxk.dedyn.io',
+	'proxyip.multacom.fxxk.dedyn.io',
+	'proxyip.vultr.fxxk.dedyn.io',
 ];
 let CMproxyIPs = [
 	//{ proxyIP: "proxyip.fxxk.dedyn.io", type: "HK" },
